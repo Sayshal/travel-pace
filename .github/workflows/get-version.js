eefb4fcb08802da8cb7387d888195ad30a5892b5
Tyler <3deathsaves@gmail.com>
2025-08-20 16:02:55 -0600

    Build changes

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -5,3 +5,6 @@ console.log('VERSION:' + moduleJson.version);
 console.log('COMPAT_MIN:' + moduleJson.compatibility.minimum);
 console.log('COMPAT_VERIFIED:' + moduleJson.compatibility.verified);
 console.log('COMPAT_MAX:' + moduleJson.compatibility.maximum);
+console.log('MODULE_ID:' + moduleJson.id);
+console.log('MODULE_TITLE:' + moduleJson.title);
+console.log('MODULE_URL:' + moduleJson.url);
